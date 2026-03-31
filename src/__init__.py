236bd8a95290c3a3fa1509278093bfecf00439d6
Pradyot <148654959+Pradyot14@users.noreply.github.com>
2026-04-01 08:59:10 +0900

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# FileNo Extractor
-from .macro_extractor import MacroExtractor
-from .ast_parser import ASTParser
-from .data_flow_analyzer import DataFlowAnalyzer
-
-__all__ = ['MacroExtractor', 'ASTParser', 'DataFlowAnalyzer']
-__version__ = '1.0.0'
